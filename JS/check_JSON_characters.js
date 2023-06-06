f8abd206471c8820fcfddffd924b8b792fa028f0
J. Cravens <jaycrav3ns@gmail.com>
2023-06-06 16:16:24 -0500

    Add files via upload

---
+++ JS/check_JSON_characters.js
@@ -0,0 +1,10 @@
+﻿//Check JSON for bad characters
+
+function VerifyInput(input){
+    for(var x=0; x<input.length; ++x){
+        let c = input.charCodeAt(x);
+        if(c >= 0 && c <= 31){
+            throw 'problematic character found at position ' + x;
+        }
+    }
+}
