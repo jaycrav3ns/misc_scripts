c40299c0cf3fb86ffcf2daf108bc15f3bb1185c4
J. Cravens <jaycrav3ns@gmail.com>
2023-07-09 18:03:14 -0500

    Add files via upload

---
+++ shellscripts/img4vid.sh
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/bash
+
+# Still image in place of a video stream
+# Usage: ./img4vid.sh image.png audio.aac
+
+ffmpeg -i "$1" -i "$2" \
+	-loop 1 -vf "scale='min(1280,iw)':-2,format=yuv420p" \
+	-c:v libx264 -preset medium -profile:v main \
+	-c:a aac -shortest -movflags +faststart \
+	output.mp4
